ce803a43dde67fb505e6ea52c3c9a3f4f8f1b811
Paul Hammond <paul.hammond@bbc.co.uk>
2017-12-02 22:28:51 +0000

    tidy up 01/solution-01

--- src/02/solution-01.js
+++ src/02/solution-01.js
@@ -1,8 +1,4 @@
 const rows = input => input.split(/\n/).map(row => row.split(/\t/).map(Number))
 
-module.exports = input => {
-  return rows(input).reduce((accumulator, current) => {
-    const difference = Math.max(...current) - Math.min(...current)
-    return accumulator + difference
-  }, 0)
-}
+module.exports = input =>
+  rows(input).reduce((accumulator, current) => accumulator + Math.max(...current) - Math.min(...current), 0)
