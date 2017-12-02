8628db1bdee4463c3ccd6e0f23a208158ae9c657
Paul Hammond <paul.hammond@bbc.co.uk>
2017-12-02 21:07:22 +0000

    remove unecessary parameters)

--- src/02/solution-01.js
+++ src/02/solution-01.js
@@ -1,7 +1,7 @@
 const rows = input => input.split(/\n/).map(row => row.split(/\t/).map(Number))
 
 module.exports = input => {
-  return rows(input).reduce((accumulator, current, index, original) => {
+  return rows(input).reduce((accumulator, current) => {
     const difference = Math.max(...current) - Math.min(...current)
     return accumulator + difference
   }, 0)
