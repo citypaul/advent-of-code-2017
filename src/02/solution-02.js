db35a0e2bcb176c6708a3bbf05983b276f93bb11
Paul Hammond <paul.hammond@bbc.co.uk>
2017-12-02 20:58:01 +0000

    small refactoring

--- src/02/solution-02.js
+++ src/02/solution-02.js
@@ -1,23 +1,16 @@
 const rows = input => input.split(/\n/).map(row => row.split(/\t/).map(Number))
-/*
-5	9	2	8 (4)
-9	4	7	3 (3)
-3	8	6	5 (2) 
-4+ 3 + 2 = 9
+const divisible = (row, number) =>
+  row.find(element => {
+    return number % element === 0 && element !== number
+  })
 
-They would like you to find those numbers on each line, divide them, and add up each line's result.
-*/
 module.exports = input => {
-  const result = rows(input).reduce((acc, row, index) => {
-    const test = row.reduce((acc, number, ind, arr) => {
-      const found = arr.find(element => {
-        return number % element === 0 && element !== number
-      })
-      return (acc += found ? number / found : 0)
-    }, 0)
-
-    return acc + test
+  return rows(input).reduce((sum, row) => {
+    return (
+      sum +
+      row.reduce((sum, number) => {
+        return (sum += divisible(row, number) ? number / divisible(row, number) : 0)
+      }, 0)
+    )
   }, 0)
-
-  return result
 }
