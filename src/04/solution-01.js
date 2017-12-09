3095e8fd85c93dfc80baeef45821874a8629e0fb
Paul Hammond <paul.hammond@bbc.co.uk>
2017-12-09 12:01:47 +0000

    use R.compose

--- src/04/solution-01.js
+++ src/04/solution-01.js
@@ -2,14 +2,7 @@ const R = require('ramda')
 
 const isUniq = input => R.uniq(input).length === input.length
 const lines = input => input.split('\n').map(line => line.split(' '))
-const countUnique = input => {
-  return lines(input).reduce((acc, curr) => {
-    return acc + isUniq(curr)
-  }, 0)
-}
+const countUnique = input => input.reduce((acc, curr) => acc + isUniq(curr), 0)
+const uniqueLines = R.compose(countUnique, lines)
 
-const uniqueLines = R.compose(isUniq, lines)
-
-module.exports = input => countUnique(input)
-
-//input => isUniq(input.split(' '))
+module.exports = input => uniqueLines(input)
