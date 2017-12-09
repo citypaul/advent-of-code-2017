aa85c1c29bdc2ca63623171c787aa6696db16bc8
Paul Hammond <paul.hammond@bbc.co.uk>
2017-12-09 15:41:19 +0000

    curry

--- src/04/solution-01.js
+++ src/04/solution-01.js
@@ -2,7 +2,7 @@ const R = require('ramda')
 
 const isUniq = input => R.uniq(input).length === input.length
 const lines = input => input.split('\n').map(line => line.split(' '))
-const countUnique = input => input.reduce((acc, curr) => acc + isUniq(curr), 0)
-const uniqueLines = R.compose(countUnique, lines)
+const countBy = (by, input) => input.reduce((acc, curr) => acc + by(curr), 0)
+const uniqueLines = R.compose(R.curry(countBy)(isUniq), lines)
 
 module.exports = input => uniqueLines(input)
